c3f4c0c836cadaa636fa6cbdba2aeb43b0d507ec
nxf43614 <gyanedra.kumar@nxp.com>
2022-09-11 01:40:13 +0530

    graph implementation
    add graph

---
+++ src/main/java/com/power/PowerCalculator.java
@@ -0,0 +1,22 @@
+package com.power;
+
+public class PowerCalculator {
+	public static void main(String[] args) {
+//		long base = 12;
+//		long power = 12;
+//		long ans = 1;
+//		while (power > 0) {
+//			if ((power & 1) == 1) {
+//				// if == 0 there is no point to calculate ans
+//				ans = ans * base;
+//			}
+//			base = base * base;
+//			// keep dividing power by 2 using right shift
+//			power = power >> 1;
+//		}
+		//System.out.println(ans);
+		
+		System.out.println("2>>2 "+(5 << 2));
+	}
+
+}
